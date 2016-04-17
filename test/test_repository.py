f274113efcc9d6dfaf7966516d0795a1465ede04
Jeff Zohrab <jzohrab@gmail.com>
2016-04-17 18:15:56 -0400

    Refactor test.

--- test/test_repository.py
+++ test/test_repository.py
@@ -5,13 +5,15 @@ from event.model import Talk
 
 class Talk_Tests(unittest.TestCase):
 
+    def setUp(self):
+        self.repo = Repository()
+
     def test_smoke_test_can_save_and_retrieve_a_talk(self):
         t1 = Talk()
         t1.title = 'Supertalk'
         t1.speaker = 'John Smith'
-        r = Repository()
-        r.save(t1)
-        t = r.get_talk('Supertalk')
+        self.repo.save(t1)
+        t = self.repo.get_talk('Supertalk')
         self.assertEqual('Supertalk',t.title)
         self.assertEqual('John Smith',t.speaker)
 
