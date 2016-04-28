e7ae78c967cda694d5c57d470e6e33fee2919734
Jeff Zohrab <jzohrab@gmail.com>
2016-04-27 22:51:51 -0400

    Re-baselining schema.
    Db schema requires manual tweaks to actually work on rebuild.

--- schema/baseline_schema/Db.sql
+++ schema/baseline_schema/Db.sql
@@ -1,12 +1,20 @@
---
--- MySQL db schema, created by hand initially
---
+CREATE TABLE __schema_migrations (
+  migration_id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  script_name varchar(255) DEFAULT NULL,
+  date_applied timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (migration_id),
+  UNIQUE KEY migration_id (migration_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Table structure for table talk
+--
 
 CREATE TABLE talk (
-    talk_id mediumint not null auto_increment, 
-    talk_title varchar(100),
-    talk_speaker varchar(100),
-    primary key (talk_id)
-);
-
+  talk_id mediumint(9) NOT NULL AUTO_INCREMENT,
+  talk_title varchar(100) DEFAULT NULL,
+  talk_speaker varchar(100) DEFAULT NULL,
+  talk_minutes int(11) NOT NULL,
+  PRIMARY KEY (talk_id),
+  UNIQUE KEY talk_title (talk_title)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
