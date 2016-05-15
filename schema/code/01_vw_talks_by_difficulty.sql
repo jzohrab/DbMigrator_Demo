431fbda7999615f111a038d01c18430d3a697ee8
Jeff Zohrab <jzohrab@gmail.com>
2016-05-14 22:04:39 -0400

    pending_20160901_drop_obsolete_talk_speaker compat fixes.

--- schema/code/01_vw_talks_by_difficulty.sql
+++ schema/code/01_vw_talks_by_difficulty.sql
@@ -4,6 +4,6 @@ as
 
 select
 
-t.talk_id, t.talk_title, t.talk_speaker, t.talk_minutes, d.difficulty_name
+t.talk_id, t.talk_title, t.talk_speaker_last, t.talk_minutes, d.difficulty_name
 from talk t inner join difficulty d on d.difficulty_id = t.difficulty_id
-order by d.difficulty_sort desc, t.talk_speaker
+order by d.difficulty_sort desc, t.talk_speaker_last
